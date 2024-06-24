b313736521a9df6711b36502b1e4d70220d10593
Sahan Nimantha <e19205@eng.pdn.ac.lk>
2024-06-24 23:03:23 +0530

    Update

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,14 +1,21 @@
 import "./app.scss";
+// import Navbar from "./components/navbar/navbar";
+import Test from "./Test";
 
 const App = () => {
   return <div>
+    {/* <section>
+      <Navbar />
+    </section>
     <section>Hero</section>
     <section>Intro</section>
-    <section>Servisdsdces</section>
+    <section>Services</section>
     <section>Process</section>
     <section>Contacts</section>
     <section>Reviews</section>
-    <section>Footer</section>
+    <section>Footer</section> */
+    }
+    <Test />
   </div>;
 };
 
---
+++ Frontend/src/components/Navbar/navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './navbar.scss'
+
+const navbar = () => {
+    return (
+        <div className='navbar'>
+            {/*Sidebar */}
+            <div className="wrapper">
+            </div>
+        </div>
+    )
+}
+
+export default navbar
